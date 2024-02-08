3a2264c8ac916173820607df51bb5ca0d283f40d
Lucas Lui Motta <lucas.lui_citsoftware@prestador.globo>
2024-02-08 13:32:23 -0300

    feat: improve article thumbnail

--- src/components/article-card/article-card.tsx
+++ src/components/article-card/article-card.tsx
@@ -6,35 +6,35 @@ import styles from './article-card.module.scss'
 import { getDateFormat } from '@/utils/date'
 
 type Props = {
-  title: string
-  description: string
-  content: string
-  imageUrl: string
-  userId: string
-  categoryId: string
-  createdAt: string
+    title: string
+    description: string
+    content: string
+    imageUrl: string
+    userId: string
+    categoryId: string
+    createdAt: string
 }
 
 const ArticleCard: React.FC<Props> = (props: Props) => {
-  return (
-    <div className={styles.article_card}>
-      <Image src={props.imageUrl} alt='Imagem do Artigo' width={100} height={100}/>
-      <div className={styles.article_body}>
-        <h4>{props.title}</h4>
-        <span>{props.description}</span>
-      </div>
-      <div className={styles.article_footer}>
-        <div>
-          <i className="far fa-calendar-alt" />
-          <span>{getDateFormat(props.createdAt)}</span>
+    return (
+        <div className={styles.article_card}>
+            <Image src={props.imageUrl} alt='Imagem do Artigo' width={240} height={172} objectFit='cover' />
+            <div className={styles.article_body}>
+                <h4>{props.title}</h4>
+                <span>{props.description}</span>
+            </div>
+            <div className={styles.article_footer}>
+                <div>
+                    <i className="far fa-calendar-alt" />
+                    <span>{getDateFormat(props.createdAt)}</span>
+                </div>
+                <div>
+                    <i className="fas fa-user-edit" />
+                    <span>{'Lucas'}</span>
+                </div>
+            </div>
         </div>
-        <div>
-          <i className="fas fa-user-edit" />
-          <span>{'Lucas'}</span>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default ArticleCard
