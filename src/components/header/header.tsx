0883b2dbe53249a4502e0cbf5605698391243d85
Lucas Lui Motta <lucas.lui_citsoftware@prestador.globo>
2024-02-11 20:58:17 -0300

    fix: remove use window size hook from header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,18 +7,15 @@ import Image from 'next/image'
 import styles from './header.module.scss'
 
 import LogoImage from '../../assets/imgs/logo.svg'
-import useWindowSize from '@/hooks/use-window-size'
 
 const Header: React.FC = () => {
-
-    const windowSize = useWindowSize()
 
     return (
         <header className={styles.header}>
             <Link href='/' className={styles.logo}>
                 <Image src={LogoImage} alt="logo" />
                 <main>
-                    <h1> &lt; <span> {windowSize.width <= 700 ? 'IoT P-Library' : 'Internet of Things P-Library'} </span>/&gt; </h1>
+                    <h1> &lt; <span> Internet of Things P-Library </span>/&gt; </h1>
                     <h2>
                         Aiming to disseminate knowledge about the Internet of Things in a simple, practical and objective way
                     </h2>
