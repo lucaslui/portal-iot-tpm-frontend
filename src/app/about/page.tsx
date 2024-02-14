fc96c84295e68efd617f98c1f3e403bc04627e48
Lucas Lui Motta <lucas.lui_citsoftware@prestador.globo>
2024-02-13 23:46:43 -0300

    fix: contact me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -22,7 +22,7 @@ const About: React.FC = () => {
                     <br /><br />
 
                     The project was created by the developer and IoT enthusiast
-                    <a href="https://github.com/lucaslui" target="_blank"> Lucas Lui Motta </a>
+                    <a href="https://br.linkedin.com/in/lucas-lui-motta" target="_blank"> Lucas Lui Motta </a>
                     who loves to debate on the subject and believes that there is no single truth that cannot be discussed or questioned.
 
                     <br /><br />
@@ -35,14 +35,10 @@ const About: React.FC = () => {
                     Various content related to IoT such as concepts, guides, projects and news will be published.
                     Starting from adoptions of conceptual reference models to discussions about business, requirements and technology.
                 </p>
-                <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="lucas-lui-motta" data-version="v1">
-                    <a className="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/lucas-lui-motta?trk=profile-badge"></a>
-                </div>
             </section>
             <h2>
                 Feel free to contact me if you have any questions or suggestions.
             </h2>
-            <Script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript" />
         </div>
     )
 }
