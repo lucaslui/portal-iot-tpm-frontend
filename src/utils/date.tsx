4e786794db0bc890f5797057dca89e1e8e9f907d
Lucas Lui Motta <lucas.lui_citsoftware@prestador.globo>
2024-02-22 16:30:08 -0300

    feat: improve article card style

--- src/utils/date.tsx
+++ src/utils/date.tsx
@@ -13,18 +13,18 @@ export const getLongDateFormat = (date: Date | string): string => {
     const dayConverted = day < 10 ? `0${day}` : day
     const month = ISODate.getMonth() + 1
     const months = [
-        'Janeiro',
-        'Fevereiro',
-        'Março',
-        'Abril',
-        'Maio',
-        'Junho',
-        'Julho',
-        'Agosto',
-        'Setembro',
-        'Outubro',
-        'Novembro',
-        'Dezembro'
+        'janeiro',
+        'fevereiro',
+        'março',
+        'abril',
+        'maio',
+        'junho',
+        'julho',
+        'agosto',
+        'setembro',
+        'outubro',
+        'novembro',
+        'dezembro'
     ]
     return `${dayConverted} de ${months[month]} de ${ISODate.getFullYear()}`
 }
