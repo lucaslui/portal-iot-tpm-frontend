d85128bba7c54723b2b62c1a804a037928da1668
Lucas Lui Motta <lucasluimotta@gmail.com>
2024-06-27 10:12:23 -0300

    feat: separate home contents by type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import styles from './page.module.scss'
 
 import { ArticleViewModel } from '@/model/article'
 import PostCardFlexible from '@/components/article-card-flexible/post-card-flexible'
+import Link from 'next/link'
+import NewsCardFlexible from '@/components/news-card-flexible/news-card-flexible'
 
 const loadPosts = async (): Promise<ArticleViewModel[]> => {
     const result = await fetch(`${process.env.API_URL}/api/articles`, {
@@ -21,15 +23,43 @@ const HomePage: React.FC = async () => {
 
     return (
         <div className={styles.home}>
-            <div className={styles.feed}>
-                {
-                    articles.map((article: ArticleViewModel) => {
-                        return (
-                            <PostCardFlexible key={article.id} article={article} />
-                        )
-                    })
-                }
-                <div className={styles.spacer}></div>
+            <div className={styles.news}>
+                <div className={styles.title}>
+                    <h3>Últimas Noticias</h3>
+                    <Link href="/concepts">Ver todos</Link>
+                </div>
+                <div className={styles.feed}>
+                    {
+                        articles
+                            .filter(article => article.type === 'news')
+                            .slice(0, 6)
+                            .map((article: ArticleViewModel) => {
+                                return (
+                                    <NewsCardFlexible key={article.id} article={article} />
+                                )
+                            })
+                    }
+                    <div className={styles.spacer}></div>
+                </div>
+            </div>
+            <div className={styles.concepts}>
+                <div className={styles.title}>
+                    <h3>Últimos Artigos</h3>
+                    <Link href="/concepts">Ver todos</Link>
+                </div>
+                <div className={styles.feed}>
+                    {
+                        articles
+                            .filter(article => article.type === 'concepts')
+                            .slice(0, 6)
+                            .map((article: ArticleViewModel) => {
+                                return (
+                                    <PostCardFlexible key={article.id} article={article} />
+                                )
+                            })
+                    }
+                    <div className={styles.spacer}></div>
+                </div>
             </div>
         </div>
     )
