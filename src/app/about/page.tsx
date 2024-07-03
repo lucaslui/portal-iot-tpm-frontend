bed85a05476d082e1257f4125340ace687232c95
Lucas Lui Motta <lucasluimotta@gmail.com>
2024-07-03 19:09:08 -0300

    fix: about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,23 +19,23 @@ const AboutPage: React.FC = () => {
 
                     <br /><br />
 
-                    O projeto foi iniciado pelo desenvolvedor e entusiasta <a href="https://br.linkedin.com/in/lucas-lui-motta" target="_blank">Lucas Lui Motta</a>, que adora debater sobre o assunto e acredita no poder do ensino baseado em projetos.
+                    O portal foi iniciado pelo desenvolvedor e entusiasta <a href="https://br.linkedin.com/in/lucas-lui-motta" target="_blank">Lucas Lui Motta</a>, que adora debater sobre o assunto e acredita no poder do ensino baseado em projetos.
 
                     <br /><br />
 
-                    Os conteúdos encontrados na biblioteca estão divididos por conceitos, tutoriais, projetos e notícias.
+                    Os conteúdos encontrados no portal estão divididos por conceitos, tutoriais, projetos e notícias.
 
-                    <br /><br />
+                    {/* <br /><br />
 
                     Para iniciar os estudos, recomendamos a leitura de <b>Introdução à Internet das Coisas</b>:
-                </p>
-                <ul>
+                </p> */}
+                {/* <ul>
                     <li><a href="/concepts/65c41bac7750e3ea07c83e69">Parte I</a></li>
                     <li><a href="/concepts/65c641aee21e7ad9a8e43f7d">Parte II</a></li>
                     <li><a href="/concepts/65c641b3e21e7ad9a8e43f7e">Parte III</a></li>
                     <li><a href="/concepts/65e0fdae7e49bb7e28d6b787">Parte IV</a></li>
                     <li><a href="/concepts/65e0fdae7e49bb7e28d6b787">Parte V</a></li>
-                </ul>
+                </ul> */}
                 {/* <p>
                     Boa parte do conteúdo apresentado é fundamentado em minha opinião, a qual se baseia em experiências pessoais, além de referências devidamente citadas.
                 </p> */}
