ecbf3bf374a07d2f28bb2f3e3dcd148d0ed1f024
Lucas Lui Motta <lucasluimotta@gmail.com>
2024-07-26 14:33:08 -0300

    feat: add services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -11,38 +11,59 @@ const poppins = Poppins({
 
 const ServicesPage: React.FC = () => {
     return (
-        <div className={styles.services_page}>
-            <h1> Sobre o projeto </h1>
-            <section className={`${poppins.className}`}>
+        <div className={`${poppins.className} ${styles.services_page}`}>
+            <h1> Serviços </h1>
+            <p>
+                A comunidade <strong>IoT-Tpm Unicamp</strong>, em comunhão com o laboratório do <strong>WissTek</strong>, oferece diversos serviços para a comunidade acadêmica e empresarial.
+            </p>
+            <section>
+                <h2>Ensino</h2>
                 <p>
-                    O principal objetivo do projeto é disseminar conhecimento sobre Internet das Coisas de forma simples, prática e objetiva.
-
-                    <br /><br />
-
-                    O projeto foi iniciado pelo desenvolvedor e entusiasta <a href="https://br.linkedin.com/in/lucas-lui-motta" target="_blank">Lucas Lui Motta</a>, que adora debater sobre o assunto e acredita no poder do ensino baseado em projetos.
-
-                    <br /><br />
-
-                    Os conteúdos encontrados na biblioteca estão divididos por conceitos, tutoriais, projetos e notícias.
-
-                    <br /><br />
-
-                    Para iniciar os estudos, recomendamos a leitura de <b>Introdução à Internet das Coisas</b>:
+                    Nosso compromisso com a educação se reflete na oferta de disciplinas especializadas e projetos de iniciação científica, proporcionando uma formação sólida e atualizada em tecnologias emergentes.
                 </p>
                 <ul>
-                    <li><a href="/concepts/65c41bac7750e3ea07c83e69">Parte I</a></li>
-                    <li><a href="/concepts/65c641aee21e7ad9a8e43f7d">Parte II</a></li>
-                    <li><a href="/concepts/65c641b3e21e7ad9a8e43f7e">Parte III</a></li>
-                    <li><a href="/concepts/65e0fdae7e49bb7e28d6b787">Parte IV</a></li>
-                    <li><a href="/concepts/65e0fdae7e49bb7e28d6b787">Parte V</a></li>
+                    <li> <strong>Disciplina Tópicos em Comunicações VI (IE-309):</strong> Explorando os fundamentos e avanços nas comunicações modernas.</li>
+                    <li> <strong>Projetos de Iniciação Científica:</strong> Incentivando a pesquisa e a inovação desde o início da carreira acadêmica.</li>
                 </ul>
-                {/* <p>
-                    Boa parte do conteúdo apresentado é fundamentado em minha opinião, a qual se baseia em experiências pessoais, além de referências devidamente citadas.
-                </p> */}
             </section>
-            <h3>
-                Sinta-se à vontade para entrar em contato se tiver alguma dúvida ou sugestão.
-            </h3>
+            <section>
+                <h2>Pesquisa Científica</h2>
+                <p>
+                    Nosso foco em pesquisa científica busca avançar o conhecimento nas áreas de Internet das Coisas (IoT), Blockchain, Inteligência Artifial (AI) e Redes de Sensores sem Fio (RSSF), contribuindo para o desenvolvimento de novas tecnologias e soluções inovadoras.
+                </p>
+                <ul>
+                    <li><strong>IoT:</strong> Investigação de novas aplicações e tecnologias para conectar dispositivos e sistemas.</li>
+                    <li><strong>IoT e Blockchain:</strong> Investigação da integração de IoT com Blockchain para criar soluções seguras e automatizadas.</li>
+                    <li><strong>IoT e AI:</strong> Investigação de IoT com IA para aumentar o conhecimento e extrair o máximo os dados gerados pelo IoT.</li>
+                    <li><strong>IoT e RSSF:</strong> Investigação de redes eficientes e escaláveis para suportar a comunicação entre dispositivos IoT.</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Cursos de Extensão</h2>
+                <p>
+                    Nossos cursos de extensão são projetados para fornecer conhecimentos práticos e teóricos sobre as tecnologias mais recentes, capacitando profissionais para enfrentar os desafios do mercado.
+                </p>
+                <ul>
+                    <li><strong>Metodologia para criar soluções IoT-TpM:</strong> Abordagens práticas para desenvolver soluções IoT eficientes.</li>
+                    <li><strong>IoT & Blockchain para suporte a contratos inteligente (FEE-0244):</strong> Como utilizar Blockchain para melhorar a segurança e a eficiência em sistemas IoT.</li>
+                    <li><strong>Redes de Comunicação sem Fio para Internet das coisas (FEE-0230):</strong> Princípios e técnicas para implementar redes de comunicação robustas para IoT.</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Consultoria</h2>
+                <p>
+                    Oferecemos serviços de consultoria especializados para empresas que buscam implementar ou aprimorar suas soluções de IoT, Blockchain e RSSF, garantindo a entrega de projetos inovadores e bem-sucedidos.
+                </p>
+                <ul>
+                    <li><strong>Desenvolvimento de soluções IoT:</strong> Consultoria completa para criação e implementação de soluções IoT personalizadas.</li>
+                    <li><strong>Desenvolvimento de soluções IoT, Blockchain e Contrato Inteligente:</strong> Integração de tecnologias emergentes para aumentar a segurança e a eficiência.</li>
+                    <li><strong>Desenvolvimento de soluções IoT e Inteligência Artificial:</strong> Utilização de técnicas de IA para aumentar o nível de conhecimento dos dados gerados por sistemas IoT.</li>
+                    <li><strong>Desenvolvimento de soluções RSSF para IoT:</strong> Projetos de redes de sensores sem fio eficientes e escaláveis para suportar diversas aplicações IoT.</li>
+                </ul>
+            </section>
+            <p>
+                Nosso compromisso é fornecer serviços de alta qualidade que atendam às necessidades específicas de nossos clientes, promovendo inovação e excelência em cada projeto. A colaboração com a comunidade acadêmica e empresarial nos permite estar na vanguarda das tecnologias emergentes, garantindo que nossos serviços sejam sempre relevantes e impactantes.
+            </p>
         </div>
     )
 }
